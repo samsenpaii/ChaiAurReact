e01839ce3d5109a985d933d71a244b89968d13f0
Samarth <samarthpatil2021.comp@mmcoe.edu.in>
2024-06-07 09:51:14 +0530

    context api

---
+++ 06contextApi/src/context/UserContextProvider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserContext from './UserContext'
+import { useState } from 'react'
+function UserContextProvider({children}) {
+    const [user , setUser] = useState(null)
+  return (
+    <UserContext.Provider value={{user , setUser}}>
+        {children}
+    </UserContext.Provider>
+  )
+}
+
+export default UserContextProvider
